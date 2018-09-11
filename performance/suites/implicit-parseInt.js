2ad58ae7872a2d33ac3f6803cacf798605cfb48a
Daniel Berlanga <daniel.berlanga@rakuten.com>
2018-09-11 11:42:07 +0900

    Added new results to README.md

--- performance/suites/implicit-parseInt.js
+++ performance/suites/implicit-parseInt.js
@@ -13,15 +13,27 @@ const seconds = '56';
  * testers
  */
 function manual() {
-  Date.UTC(parseInt(year), parseInt(month), parseInt(day), parseInt(hours), parseInt(minutes), parseInt(seconds));
+  for (let i = 0; i < N; i++) {
+    Date.UTC(parseInt(year), parseInt(month), parseInt(day), parseInt(hours), parseInt(minutes), parseInt(seconds));
+  }
 }
 
 function manualBase() {
-  Date.UTC(parseInt(year, 10), parseInt(month, 10), parseInt(day, 10), parseInt(hours, 10), parseInt(minutes, 10), parseInt(seconds, 10));
+  for (let i = 0; i < N; i++) {
+    Date.UTC(parseInt(year, 10), parseInt(month, 10), parseInt(day, 10), parseInt(hours, 10), parseInt(minutes, 10), parseInt(seconds, 10));
+  }
+}
+
+function number() {
+  for (let i = 0; i < N; i++) {
+    Date.UTC(Number(year), Number(month), Number(day), Number(hours), Number(minutes), Number(seconds));
+  }
 }
 
 function auto() {
-  Date.UTC(year, month, day, hours, minutes, seconds);
+  for (let i = 0; i < N; i++) {
+    Date.UTC(year, month, day, hours, minutes, seconds);
+  }
 }
 
 /*
@@ -29,8 +41,9 @@ function auto() {
  */
 const suite = new Benchmark.Suite('Implicit vs explicit parseInt conversion');
 
-suite.add('manual', manual)
-     .add('manualBase', manualBase)
+suite.add('parseInt(x)', manual)
+     .add('parseInt(x, 10)', manualBase)
+     .add('Number(x)', number)
      .add('auto', auto);
 
 module.exports = suite;
