fe805808ade08747ff4dacb8d95726aaa2cbd3a7
danikaze <danikaze@gmail.com>
2019-04-24 16:49:53 +0900

    Add test results on node 10.15.3

--- performance/suites/string-concat.js
+++ performance/suites/string-concat.js
@@ -16,12 +16,12 @@ function singleQuotesConcatenation() {
 }
 
 function doubleQuotesConcatenation() {
-  return 'Lorem ipsum dolor sit amet,' +
-         ' consectetur adipiscing elit.' +
-         ' Mauris rhoncus felis in tortor' +
-         ' tristique posuere.' +
-         ' In viverra ut nunc non fringilla.' +
-         ' Nam ullamcorper venenatis pulvinar.' +
+  return "Lorem ipsum dolor sit amet," +
+         " consectetur adipiscing elit." +
+         " Mauris rhoncus felis in tortor" +
+         " tristique posuere." +
+         " In viverra ut nunc non fringilla." +
+         " Nam ullamcorper venenatis pulvinar." +
          str + str + str + str + str + str;
 }
 
